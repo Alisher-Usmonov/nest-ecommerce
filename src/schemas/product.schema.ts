67646c966fd83fdc3584d9a76ab55e2e055cfbee
Alisher-Usmonov <alisherufullstack@gmail.com>
2022-06-17 12:17:05 +0500

    product

---
+++ src/schemas/product.schema.ts
@@ -0,0 +1,28 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+import { v4 } from "uuid";
+
+export type ProductDocument = Document | Product;
+
+@Schema()
+export class Product {
+    @Prop({ required: true, unique: true, default: v4() })
+    id: string;
+
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ required: true })
+    slug: string;
+
+    @Prop({ required: true })
+    price: number;
+
+    @Prop({ required: true })
+    description: string;
+
+    @Prop({ required: true })
+    image_url: string;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
